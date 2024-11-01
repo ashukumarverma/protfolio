ec279fb39c393e70fea9a88402af1c047fefd410
Ashutosh <ashukumar3436@gmail.com>
2024-11-01 21:44:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <p>Hi there! I am</p>
+        <p>Ashutosh Kumar</p>
+        <p>a MERN stack developer</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
